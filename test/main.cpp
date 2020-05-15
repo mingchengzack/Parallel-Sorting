9ee0227e9cc5e98dc1d2c0e9a80d44ed2ff8cb99
mingchengzack <chengmingjtt123@gmail.com>
2020-05-14 21:01:43 -0700

    serial bitonic sort

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,7 @@
 
 int main() {
   std::vector<int> arr{4, 2, 1, 100, 3, 9, 1000, 5, 7, -1, -2};
-  quickSort(arr);
+  mergeSort(arr);
   for (int i = 0; i < arr.size(); i++) {
     std::cout << arr[i] << ",";
   }
