70250c261e4e6b183b76bae993c8e5ef771228ad
Ming Cheng <mic002@comet-27-47.sdsc.edu>
2020-05-15 18:40:10 -0700

    Optimize parallel bitnoic sort

--- parallel/bitonicSort_parallel.cpp
+++ parallel/bitonicSort_parallel.cpp
@@ -26,9 +26,45 @@ int greatestPowerOfTwoLessThan(int cnt) {
   return k >> 1;
 }
 
-// Merge two sequences
-void bitonicMerge(vector<int> &arr, int l, int cnt, bool dir, int p) {
+// Serial version of merging two bitonic sequences
+void bitonicMerge(vector<int> &arr, int l, int cnt, bool dir) {
   if (cnt <= 1) {
+    return;
+  }
+
+  int k = greatestPowerOfTwoLessThan(cnt);
+
+  for (int i = l; i < l + cnt - k; i++) {
+    compSwap(arr, i, i + k, dir);
+  }
+
+  bitonicMerge(arr, l, k, dir);
+  bitonicMerge(arr, l + k, cnt - k, dir);
+}
+
+// Serial version of bitonic sort
+void bitonicSortHelper(std::vector<int> &arr, int l, int cnt, bool dir) {
+  // Base case
+  if (cnt <= 1) {
+    return;
+  }
+  
+  int k = cnt / 2;
+
+  // Sort in ascending order
+  bitonicSortHelper(arr, l, k, !dir);
+
+  // Sort in descending order
+  bitonicSortHelper(arr, l + k, cnt - k, dir);
+
+  // Merge sequence in ascending order
+  bitonicMerge(arr, l, cnt, dir);
+}
+
+// Parallel version of merging two bitonic sequences
+void openmp_bitonicMerge(vector<int> &arr, int l, int cnt, bool dir, int p) {
+  if (cnt <= 200) {
+    bitonicMerge(arr, l, cnt, dir);
     return;
   }
 
@@ -44,43 +80,50 @@ void bitonicMerge(vector<int> &arr, int l, int cnt, bool dir, int p) {
 
     #pragma omp single
     {
-      #pragma omp task if(cnt > 1024)
-        bitonicMerge(arr, l, k, dir, p);
+      #pragma omp task
+        openmp_bitonicMerge(arr, l, k, dir, p);
 
-      #pragma omp task if(cnt > 1024)
-        bitonicMerge(arr, l + k, cnt - k, dir, p);
+      #pragma omp task
+        openmp_bitonicMerge(arr, l + k, cnt - k, dir, p);
     }
   }
 }
 
-void bitonicSortHelper(vector<int> &arr, int l, int cnt, bool dir, int p) {
-  // Base case
-  if (cnt <= 1) {
+// Parallel version of bitonic sort
+void openmp_bitonicSortHelper(vector<int> &arr, int l, int cnt, bool dir, int p) {
+  // If the size is less than threshold, we use serial version
+  if (cnt <= 200) {
+    bitonicSortHelper(arr, l, cnt, dir);
     return;
   }
 
   int k = cnt / 2;
 
   // Sort in ascending order
-  #pragma omp task if(cnt > 1024)
-    bitonicSortHelper(arr, l, k, !dir, p);
+  #pragma omp task
+    openmp_bitonicSortHelper(arr, l, k, !dir, p);
 
   // Sort in descending order
-  #pragma omp task if(cnt > 1024)
-    bitonicSortHelper(arr, l + k, cnt - k, dir, p);
+  #pragma omp task
+    openmp_bitonicSortHelper(arr, l + k, cnt - k, dir, p);
 
   // Merge sequence in ascending order
   #pragma omp taskwait
-  bitonicMerge(arr, l, cnt, dir, p);
+  openmp_bitonicMerge(arr, l, cnt, dir, p);
 }
 
 void bitonicSort(vector<int> &arr, int p) {
-  #pragma omp parallel num_threads(p)
-  {
-    #pragma omp single
-      cout << "Num of threads: " << omp_get_num_threads() << endl;
-    #pragma omp single
-      bitonicSortHelper(arr, 0, arr.size(), true, p);
+  if (p == 1) {
+    cout << "Num of threads: " << omp_get_num_threads() << endl;
+    bitonicSortHelper(arr, 0, arr.size(), true);
+  } else {
+    #pragma omp parallel num_threads(p)
+    {
+      #pragma omp single
+        cout << "Num of threads: " << omp_get_num_threads() << endl;
+      #pragma omp single
+        openmp_bitonicSortHelper(arr, 0, arr.size(), true, p);
+    }
   }
 }
 
